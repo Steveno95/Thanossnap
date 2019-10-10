f6991f196a75a85139b582dec5daaab037f00ff7
Steven O'Neill <oneillsteven957@gmail.com>
2019-10-10 18:06:04 -0500

    started trying to migrate the thanos snap into react
    Forgot to commit a few times so there is quite a bit in this push

--- react client/client/src/App.js
+++ react client/client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  reload() {
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="content">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button id="start-btn">Snap!</button>
+        </div>
+        <div className="outside-content">
+          <button onClick={this.reload} id="start-btn">reload</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
